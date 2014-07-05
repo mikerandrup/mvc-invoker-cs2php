c0caa35070ee356e43a6f262848905e9647bca1d
Mike Randrup <github@mikerandrup.com>
2014-07-05 17:54:01 -0500

    Basic workflow (build action to compile PHP & upload to dev server) working on simplest case (hello world).

---
+++ MvcInvoker/CompileToPhp.bat
@@ -0,0 +1,10 @@
+﻿c:
+cd \Dropbox\mvc-invoker-cs2php\MvcInvoker
+
+c:\cs2php\cs2php.exe MvcInvoker.csproj _PhpOutput
+
+subst t: \\webmercury\aecdev2
+copy _PhpOutput\*.* T:\content\cs2php_test\
+subst /d t:
+
+exit /b 0
---
+++ MvcInvoker/_PhpOutput/router.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Generated with CS2PHP
+*/
+namespace MvcInvoker {
+    class RouterPage {
+        public static function PhpMain() {
+            echo 'Hi!  This started as C# code, and was compiled into PHP.  Why does it matter?  Because static type checking rules, and I am not fluent in PHP to my own standard!';
+        }
+    }
+}
+namespace {
+    \MvcInvoker\RouterPage::PhpMain();
+}
+?>
