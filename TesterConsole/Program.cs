da12c2ff2de576a6a333e52ae5f6e312ff14311a
Mike Randrup <github@mikerandrup.com>
2014-07-06 18:45:12 -0500

    Added console app project to demo C# functionality.

---
+++ TesterConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MvcInvoker.Engine;
+
+namespace TesterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenerateResponseForPath("about/company/history"));
+            Console.WriteLine(GenerateResponseForPath("retail/karaoke4000"));
+            Console.WriteLine(GenerateResponseForPath("admin/announcements"));
+        }
+
+        static string GenerateResponseForPath(string routePath)
+        {
+            RouteCollection routeCollection = new RouteCollection();
+            RouteBase route = routeCollection.GetRouteForPath(routePath);
+            ControllerBase controller = route.CreateController();
+            return controller.GenerateResponseBody();
+        }
+    }
+}
